852efca8bef0de583652526edeeac0f5d4731fc2
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-09 11:32:47 -0400

    fixed problem, set up register, login for use and reset css

---
+++ form-management/src/components/register.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+
+export default class Register extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                hay hay
+            </div>
+        )
+    }
+}
